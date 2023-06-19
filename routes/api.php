1f80ef369f025988dc8fe6561d4822b1431ed583
nrcloudd <116167202+nrcloudd@users.noreply.github.com>
2023-06-19 20:50:08 +0700

    some changes

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/index', [AuthController::class, 'index']);
     Route::post('/show', [AuthController::class, 'show']);
+    Route::post('/update/{id}', [AuthController::class, 'show']);
     Route::post('/sewa', [AuthController::class, 'sewa']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
